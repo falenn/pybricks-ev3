d9a8861507faca175fff81faad328aaf375e5b99
falenn <dev@imwiz.com>
2021-07-25 15:14:55 +0000

    fixes for weather server

--- projects/weather/server/startup.sh
+++ projects/weather/server/startup.sh
@@ -7,12 +7,16 @@ mkdir $WORKING_DIR/mosquitto-config
 mkdir $WORKING_DIR/mosquitto-data
 mkdir $WORKING_DIR/mosquitto-log
 
-docker volume create -d local -o type=none -o bind -o device=$WORKING_DIR/config mosquitto-config
-docker volume create -d local -o type=none -o bind -o device=$WORKING_DIR/data mosquitto-data
-docker volume create -d local -o type=none -o bind -o device=$WORKING_DIR/log mosquitto-log
+docker volume create -d local -o type=none -o o=bind -o device=$WORKING_DIR/mosquitto-config mosquitto-config
+docker volume create -d local -o type=none -o o=bind -o device=$WORKING_DIR/mosquitto-data mosquitto-data
+docker volume create -d local -o type=none -o o=bind -o device=$WORKING_DIR/mosquitto-log mosquitto-log
+
+touch $WORKING_DIR/mosquitto-data/_data
+touch $WORKING_DIR/mosquitto-log/_data
+touch $WORKING_DIR/mosquitto-config/_data
 
 # config heredoc for mosquitto.conf
-cat > $WORKING_DIR/config/mosquitto.conf << EOF
+cat > $WORKING_DIR/mosquitto-config/mosquitto.conf << EOF
 persistence true
 persistence_location /opt/mosquitto/data/
 log_dest file /opt/mosquitto/log/mosquitto.log
